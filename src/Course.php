ff6574a0e222f68201978630b6e07e5f8b76e27c
Michaela Davis and Zach Swanson <michaela@>
2017-02-28 11:10:45 -0800

    pass save, getAll, and deleteAll tests

--- src/Course.php
+++ src/Course.php
@@ -1,8 +1,57 @@
 <?php
     class Course
     {
+        private $course_name;
+        private $course_number;
+        private $id;
+
+        function __construct($course_name, $course_number, $id = null)
+        {
+            $this->course_name = $course_name;
+            $this->course_number = $course_number;
+            $this->id = $id;
+        }
+
+        function getCourseName()
+        {
+            return $this->course_name;
+        }
+
+        function getCourseNumber()
+        {
+            return $this->course_number;
+        }
+
+        function getCourseId()
+        {
+            return $this->id;
+        }
+
+        function save()
+        {
+                $GLOBALS['DB']->exec("INSERT INTO courses (course_name, course_number) VALUES ('{$this->course_name}', '{$this->course_number}');");
+                $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_courses = $GLOBALS['DB']->query("SELECT * FROM courses;");
+            $all_courses = [];
+            foreach ($returned_courses as $course) {
+                $course_name = $course['course_name'];
+                $course_number = $course['course_number'];
+                $id = $course['id'];
+                $new_course = new Course($course_name, $course_number, $id);
+                array_push($all_courses, $new_course);
+            }
+            return $all_courses;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM courses;");
+        }
 
 
     }
-
 ?>
--- tests/StudentTest.php
+++ tests/StudentTest.php
@@ -3,17 +3,17 @@
     * @backupGlobals disabled
     * @backupStaticAttributes disabled
     */
-    require_once "src/Course.php";
-    require_once "src/Student.php";
-
-    $server = 'mysql:host=localhost:8889;dbname=registrar_test';
-    $username = 'root';
-    $password = 'root';
-    $DB = new PDO($server, $username, $password);
-
-
-    class StudentTest extends PHPUnit_Framework_TestCase
-    {
-
-    }
+    // require_once "src/Course.php";
+    // require_once "src/Student.php";
+    //
+    // $server = 'mysql:host=localhost:8889;dbname=registrar_test';
+    // $username = 'root';
+    // $password = 'root';
+    // $DB = new PDO($server, $username, $password);
+    //
+    //
+    // class StudentTest extends PHPUnit_Framework_TestCase
+    // {
+    //
+    // }
 ?>
